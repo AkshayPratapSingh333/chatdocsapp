84f1e65157c3b7a5894495b28e868a611f98e842
Akshay Pratap Singh <akshaypsingh333.devac@gmail.com>
2025-08-08 18:44:09 +0530

    Chat Docs

---
+++ src/lib/utils.ts
@@ -0,0 +1,22 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function validateEnvironmentVariables() {
+  const required = [
+    "GOOGLE_API_KEY",
+    "PINECONE_API_KEY",
+    "PINECONE_INDEX_NAME",
+  ];
+
+  for (const variable of required){
+    if(!process.env[variable]){
+      throw new Error(`Environment Variables Not Find : ${variable} `)
+    }
+  }
+}
